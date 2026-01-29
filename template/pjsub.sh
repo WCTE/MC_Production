3ccb4772c625c07c58902e6c386965b4af5dbd59
kmtsui <a122014@hotmail.com>
2026-01-29 17:08:30 +0900

    rename pjsub file

---
+++ template/pjsub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#PJM -L rscgrp=all              # Resource group (Queue name)
+#PJM -L elapse=24:00:00         # Time limit (24 hours)
+#PJM -o $pjout                  # Output log
+#PJM -e $pjerr                  # Error log
+
+cd $curdir
+bash $shFile
--- template/pjsub_wcsim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd $curdir
-singularity exec -u -B ./:$mntdir $siffile bash $shfile
