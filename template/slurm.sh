a1dfba5dc0407e8cee2a8fef611e32f45973741d
kmtsui <a122014@hotmail.com>
2026-03-05 13:31:30 -0800

    Update cedar/fir production code

--- template/slurm.sh
+++ template/slurm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH --account=$account
 #SBATCH --time=0-24:0:0
-#SBATCH --mem=16G
+#SBATCH --mem=16000M
 #SBATCH --output=$sout.%A.out
 #SBATCH --error=$serr.%A.err
 #SBATCH --cpus-per-task=1
+
+module load apptainer
 
 cd $curdir
 bash $shFile
